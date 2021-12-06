ea5ad668bc8d7981e93781e82894249228720fb7
Bruno Henrique <82820038+bruno535@users.noreply.github.com>
2021-12-05 22:55:44 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-<script>
-    setInterval(() => {
-    const data = new Date
-    let hora = data.toLocaleTimeString()
-    let h1 = document.querySelector('.h1')
-
-    h1.textContent=hora
-}, 1000);
-</script>
